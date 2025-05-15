446fb72778af7d651db4c2ddaa8db8b928755d3c
Pritam Dhaugoda <pritamdhaugoda@gmail.com>
2025-05-15 08:42:18 +0545

    add navbar content

---
+++ src/components/LinksDropdown.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { AlignLeft } from "lucide-react";
+import links from "../../utils/links";
+import Link from "next/link";
+
+const LinksDropdown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant="outline" size="icon">
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className="flex items-center gap-x-2">
+              {link.icon}<span className="capitalize">{link.label}</span>
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default LinksDropdown;
